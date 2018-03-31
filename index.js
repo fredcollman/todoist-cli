96299f3b13b599f904a46dce45f5abcd585cd9d5
Fred Collman <fredcollman@gmail.com>
2018-03-31 19:14:54 +0100

    Restructure basic API call

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ const fetch = require("node-fetch");
 const API_ROOT_URL = "https://beta.todoist.com/API/v8";
 const API_TOKEN = process.env.TODOIST_API_TOKEN;
 
-fetch(`${API_ROOT_URL}/projects`, {
-  headers: {
-    Authorization: `Bearer ${API_TOKEN}`
-  }
-})
-  .then(response => {
-    if (response.ok) {
-      return response.json().then(console.log);
-    } else {
-      return Promise.reject({
-        status: response.status,
-        statusText: response.statusText
-      });
+const handleBadResponse = response =>
+  Promise.reject({
+    status: response.status,
+    statusText: response.statusText
+  });
+
+const readResponse = response =>
+  response.ok ? response.json() : handleBadResponse(response);
+
+const listProjects = () =>
+  fetch(`${API_ROOT_URL}/projects`, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`
     }
-  })
-  .catch(console.error);
+  }).then(readResponse);
+
+const main = console =>
+  listProjects()
+    .then(console.log)
+    .catch(console.error);
+
+main(console);
